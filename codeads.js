20be0c179fce7aad6a75a407601a0f9f45414497
Nguyễn Mạnh Cường <74704786+cydiaguide@users.noreply.github.com>
2022-04-22 21:03:55 +0700

    Add files via upload

---
+++ codeads.js
@@ -0,0 +1,21 @@
+     <style>.axx-1, .axx-2, .axx-3, .axx-4 {
+		position: fixed;
+		z-index: 999999999;
+		text-align: center;
+		transform: translate(-50%, -50%);
+		opacity: 0;
+	}
+	.axx-1 {top: 100px; left: 40%}
+	.axx-2 {top: 300px; left: 60%}
+	.axx-3 {top: 500px; left: 20%}
+	.axx-4 {top: 700px; left: 80%}
+}</style>
+<script>
+			jQuery(document).ready(function($){
+				$('.code-1').addClass('axx-1');
+				$('.code-2').addClass('axx-2');
+				$('.code-3').addClass('axx-3');
+				$('.code-4').addClass('axx-4');
+			});
+		</script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
